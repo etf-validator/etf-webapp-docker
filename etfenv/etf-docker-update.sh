46accc5c99355f9fa26cb26c0da1e4e998b41711
Jon Herrmann <herrmann@interactive-instruments.de>
2017-06-09 09:45:29 +0200

    Update script improvements + hints

--- etfenv/etf-docker-update.sh
+++ etfenv/etf-docker-update.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Add this file to /etc/sudoers to run this file with sudo etf-docker-update.sh
-# without entering a password. Note: never edit the file with a normal
+# Add this file to /etc/sudoers . Note: never edit the file with a normal
 # text editor! Always use the visudo command instead!
 # ubuntu        ALL=(ALL)       NOPASSWD: /home/ubuntu/etfenv/etf-docker-update.sh
+#
+# Ensure this file is executable: chmod +x etf-docker-update.sh
+# For security reasons the file permissions of the docker-compose.yml file
+# should be set to -rwxr-xr-- 1 root root, so that the file can only be
+# edited as root user:
+# sudo chown root:root docker-compose.yml && sudo chmod 754 docker-compose.yml
 
 # Stop container
 /usr/local/bin/docker-compose stop
